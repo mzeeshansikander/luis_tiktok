e13848a61ac059187d2fef15a2b974248f730757
mzeeshansikander <zeeshan.sikander@live.com>
2021-05-20 17:19:56 +0500

    changed url

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
 
 import InnerHTML from "dangerously-set-html-content";
+import { useEffect, useState } from 'react';
 
 function createMarkup() {
   return { __html: "First &middot; Second" };
 }
 
-function createEmbedMarkup() {
+function createEmbedMarkup(data) {
   return {
     __html:
-      '<blockquote class="tiktok-embed" cite="https://www.tiktok.com/@scout2015/video/6718335390845095173" data-video-id="6718335390845095173" style="max-width: 605px;min-width: 325px;" > <section> <a target="_blank" title="@scout2015" href="https://www.tiktok.com/@scout2015">@scout2015</a> <p>Scramble up ur name & I’ll try to guess it😍❤️ <a title="foryoupage" target="_blank" href="https://www.tiktok.com/tag/foryoupage">#foryoupage</a> <a title="petsoftiktok" target="_blank" href="https://www.tiktok.com/tag/petsoftiktok">#petsoftiktok</a> <a title="aesthetic" target="_blank" href="https://www.tiktok.com/tag/aesthetic">#aesthetic</a></p> <a target="_blank" title="♬ original sound - tiff" href="https://www.tiktok.com/music/original-sound-6689804660171082501">♬ original sound - tiff</a> </section> </blockquote> <script async src="https://www.tiktok.com/embed.js"></script>'
+      data
   };
 }
 
+
 function App() {
+  const [data, setData] = useState([])
+  useEffect(() => {
+  fetch('https://www.tiktok.com/oembed?url=https://www.tiktok.com/@that_fooodie_girl/video/6963849640575880449?sender_device=pc&sender_web_id=6964309371824113154&is_from_webapp=v1&is_copy_url=0')
+  .then(response => response.json())
+  .then(data =>
+    setData(data)
+     );
+
+  }, [])
   return (
     <div className="App">
-      <h1>Embedding HTML</h1>
-      <InnerHTML html={createEmbedMarkup().__html} />
+      <h1>React - Embedding HTML with Script Tags</h1>
+      <InnerHTML html={createEmbedMarkup(data.html).__html} />
+
     </div>
   );
 }
